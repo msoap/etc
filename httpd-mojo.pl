bdee6fc533aaba14183fe7b416434b669e5d2f43
Sergey Mudrik <sergey.mudrik@gmail.com>
2013-12-22 21:09:30 +0200

    fix for new mojolicious version

--- httpd-mojo.pl
+++ httpd-mojo.pl
@@ -10,4 +10,4 @@ get '/' => sub {
     $self->render(text => "Hello world from perl with Mojolicious");
 };
 
-app->secret('ZdeceVW4f&32S*3dF_21')->start('daemon', listen_address => 'http://*:8080');
+app->secrets(['ZdeceVW4f&32S*3dF_21'])->start('daemon', listen_address => 'http://*:8080');
