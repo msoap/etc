622889af9c6ce899ce6b358ca7ee62b328d9c526
Sergey Mudrik <sergey.mudrik@gmail.com>
2015-03-25 00:07:34 +0200

    Speedup with custom scan algorithm for integers

--- sum-big-integers.go
+++ sum-big-integers.go
@@ -5,27 +5,87 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"math/big"
 	"os"
 	"regexp"
 )
 
-func main() {
-	re := regexp.MustCompile(`\d+`)
+const buffer_len = 1000000
+
+//-------------------------------------------------------------------
+func isNumericSymbol(symbol byte) bool {
+	switch symbol {
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return true
+	}
+	return false
+}
+
+//-------------------------------------------------------------------
+// goroutine for summation of numbers in string
+func numbersSummator(input chan []string, output chan *big.Int) {
 	i := new(big.Int)
 	sum := new(big.Int)
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		for _, integer_str := range re.FindAllString(scanner.Text(), -1) {
+	for line := range input {
+		for _, integer_str := range line {
 			i.SetString(integer_str, 10)
 			sum.Add(sum, i)
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		log.Fatalln("reading standard input error:", err)
+
+	output <- sum
+}
+
+//-------------------------------------------------------------------
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	buffer := make([]byte, buffer_len)
+	prev_str_num := ""
+
+	input := make(chan []string)
+	sum := make(chan *big.Int)
+	re := regexp.MustCompile(`\d+`)
+	go numbersSummator(input, sum)
+
+	for {
+		n, err := reader.Read(buffer)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalln("reading standard input error:", err)
+		} else if n > 0 {
+			buffer = buffer[:n]
+			nums_in_line := re.FindAllString(string(buffer), -1)
+
+			// add previous num
+			if prev_str_num != "" && isNumericSymbol(buffer[0]) {
+				nums_in_line[0] = prev_str_num + nums_in_line[0]
+			} else if prev_str_num != "" {
+				input <- []string{prev_str_num}
+			}
+
+			// save last num
+			if isNumericSymbol(buffer[n-1]) {
+				prev_str_num = nums_in_line[len(nums_in_line)-1]
+				nums_in_line = nums_in_line[:len(nums_in_line)-1]
+			} else {
+				prev_str_num = ""
+			}
+
+			if len(nums_in_line) > 0 {
+				input <- nums_in_line
+			}
+		}
 	}
 
-	fmt.Println(sum)
+	if prev_str_num != "" {
+		input <- []string{prev_str_num}
+	}
+
+	close(input)
+
+	fmt.Println(<-sum)
 }
