2a0d22413e3695f78b78dfd9adafe083f75aaaac
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-02-18 21:18:45 +0200

    Changed hello string

--- httpd_fast.go
+++ httpd_fast.go
@@ -10,7 +10,7 @@ import (
 // ----------------------------------------------------------------------------
 func main() {
 	fmt.Println("Start fasthttp on 8080")
-	responseBytes := []byte("Hello world from Go/0123456789012345678901234567890123456789012345678901234567890123456789/12")
+	responseBytes := []byte("Hello world from Go/Fasthttp/9012345678901234567890123456789012345678901234567890123456789/12")
 
 	err := fasthttp.ListenAndServe(":8080", func(ctx *fasthttp.RequestCtx) {
 		_, err := ctx.Write(responseBytes)
