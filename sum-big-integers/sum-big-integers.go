08efa9db4d94e99fcc09740088c8cfa92535af5d
Sergey Mudrik <sergey.mudrik@gmail.com>
2016-03-12 12:26:51 +0200

    Updated comment about install

--- sum-big-integers/sum-big-integers.go
+++ sum-big-integers/sum-big-integers.go
@@ -1,5 +1,11 @@
-// read all integers from stdin and out summation of it
-// echo "123 456 789" | sum-big-integers
+/*
+read all integers from stdin and out summation of it
+echo "123 456 789" | sum-big-integers
+
+Install:
+	go get -u https://github.com/msoap/etc/sum-big-integers
+
+*/
 package main
 
 import (
