3d0c0dbc77f3945ed2805f412d6d7bdf3972e2aa
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-01-14 15:00:37 +0300

    Renamed देवनागरी-में-नंबर.go script

---
+++ to-devanagari/देवनागरी-में-नंबर.go
@@ -0,0 +1,47 @@
+/*
+https://en.wikipedia.org/wiki/Devanagari#Numerals
+
+usage:
+	go run देवनागरी-में-नंबर.go "example: 2017, 01234567"
+
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var toDevanagariMap = [...]rune{
+	'०',
+	'१',
+	'२',
+	'३',
+	'४',
+	'५',
+	'६',
+	'७',
+	'८',
+	'९',
+}
+
+func toDevanagari(in string) string {
+	result := []rune{}
+	for _, c := range in {
+		d, err := strconv.Atoi(string(c))
+		if err == nil && d >= 0 && d <= 9 {
+			result = append(result, toDevanagariMap[d])
+		} else {
+			result = append(result, c)
+		}
+	}
+
+	return string(result)
+}
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		fmt.Println(toDevanagari(arg))
+	}
+}
