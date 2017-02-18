93ccf2b9db564d788706494a8995d20094f2c779
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-02-18 14:20:03 +0300

    Improved script; added build from sub directory; added create deb package

--- golang-cross-build/golang-cross-build.sh
+++ golang-cross-build/golang-cross-build.sh
@@ -1,35 +1,53 @@
 #!/bin/bash
 
-# Golang: build binaries for Linux/OS X/Windows x amd64/386/arm
+# Golang: build binaries for Linux/OS X/Windows x amd64/386/arm/arm64
 #
 # usage:
-#   golang-cross-build.sh program_name
+#   golang-cross-build.sh program_name [path]
 #
-# version get from last git tag
+#   golang-cross-build.sh program_name
+#   golang-cross-build.sh program_name ./
+#   golang-cross-build.sh program_name ./cmd/name
+#
+# Version get from last git tag
+#
+# For build *.deb need install fpm:
+#   gem install --no-ri --no-rdoc fpm
 #
 # https://github.com/msoap/etc/tree/master/golang-cross-build/
 
-# build_one_arch $name $bin_name $GOOS $GOARCH
+# build_one_arch $name $path $GOOS $GOARCH
 build_one_arch()
 {
-    name=$1
-    bin_name=$2
+    APP_NAME=$1
+    SRC_PATH=$2
     export GOOS=$3
     export GOARCH=$4
+    APP_NAME_EXE=$APP_NAME
     echo build: $GOOS/$GOARCH
-    go build -ldflags="-w" -o $2
+    go get -d -t ./...
 
     if [ $GOOS == windows ]
     then
-        zip_name="$name-$VERSION.$GOOS.$GOARCH.zip"
-        zip -9 $zip_name $bin_name README.md LICENSE
+        APP_NAME_EXE=${APP_NAME}.exe
+        go build -ldflags="-w" -o $APP_NAME_EXE $SRC_PATH
+        zip_name="$APP_NAME-$VERSION.$GOOS.$GOARCH.zip"
+        zip -9 $zip_name $APP_NAME_EXE README.md LICENSE
     else
-        zip_name="$name-$VERSION.$GOOS.$GOARCH.tar.gz"
-        tar -czf $zip_name $bin_name README.md LICENSE $(ls $bin_name.1 2>/dev/null)
+        go build -ldflags="-w" -o $APP_NAME_EXE $SRC_PATH
+        zip_name="$APP_NAME-$VERSION.$GOOS.$GOARCH.tar.gz"
+        tar -czf $zip_name $APP_NAME_EXE README.md LICENSE $(ls $APP_NAME.1 2>/dev/null)
+
+        # build deb package (need install fpm)
+        if [[ $(which fpm) ]] && [[ $GOOS == linux ]] && [[ $GOARCH == amd64 ]]; then
+            # or with docker: docker run -it --rm -v $PWD:/app -w /app ruby-fpm fpm ...
+            fpm -s dir -t deb --name $APP_NAME -v $VERSION ./$APP_NAME=/usr/bin/ ./$APP_NAME.1=/usr/share/man/man1/ LICENSE=/usr/share/doc/$APP_NAME/copyright README.md=/usr/share/doc/$APP_NAME/ && \
+              echo "$(ls *.deb) $(cat *.deb | shasum | awk '{print $1}')" >> $APP_NAME.shasum
+        fi
     fi
 
-    echo "$zip_name/$bin_name $(cat $bin_name | shasum | awk '{print $1}')" >> $name.shasum
-    rm $bin_name
+    echo "$zip_name/$APP_NAME_EXE $(cat $APP_NAME_EXE | shasum | awk '{print $1}')" >> $APP_NAME.shasum
+    rm $APP_NAME_EXE
 }
 
 VERSION=$(git tag 2>/dev/null | grep -E '^[0-9]+' | tail -1)
@@ -42,26 +60,21 @@ then
     exit 1
 fi
 
+cmd_path=${2:-./}
+
 > "$name.shasum"
 
 for GOOS in linux darwin windows
 do
     for GOARCH in amd64 386
     do
-        if [ $GOOS == windows ]
-        then
-            bin_name="$name.exe"
-        else
-            bin_name=$name
-        fi
-
-        build_one_arch $name $bin_name $GOOS $GOARCH
+        build_one_arch $name $cmd_path $GOOS $GOARCH
     done
 done
 
 # ARM
-GOARM=6 build_one_arch $name $name linux arm
-build_one_arch $name $name linux arm64
+GOARM=6 build_one_arch $name $cmd_path linux arm
+build_one_arch $name $cmd_path linux arm64
 
 # SHA sums
 cat "$name.shasum"
