b13aff86c8fb69ccf2bae36a9ca39b9b45e932bb
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-10-07 23:15:23 +0300

    Refactoring

--- gt-cli/gt-cli.go
+++ gt-cli/gt-cli.go
@@ -14,11 +14,9 @@ import (
 	"unicode"
 )
 
-const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.7"
-
-var (
-	baseURL = strings.Join([]string{"https:", "//", "translate", ".goog", "leap", "is.", "com/trans", "late_a/sin", "gle?client=g", "tx&sl=auto&tl=%s&dt=t&q=%s"}, "")
-	// baseURL = "http://localhost:8080/t?tl%s&q=%s"
+const (
+	userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.7"
+	baseURL   = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=%s&dt=t&q=%s"
 )
 
 func main() {
